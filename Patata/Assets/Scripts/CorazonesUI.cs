382027b0b499514f376b9affafff37188b642cf4
Esteban-correa <125918439+Esteban-correa@users.noreply.github.com>
2025-01-25 18:40:20 -0500

    Corazones
    Se agrega corazones

--- Patata/Assets/Scripts/CorazonesUI.cs
+++ Patata/Assets/Scripts/CorazonesUI.cs
@@ -2,57 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using System;
 using System.Linq;
 public class NewBehaviourScript : MonoBehaviour
 {
 
-    public List<Image> listaCorazones;
     public GameObject CorazonPrefab;
-    public VidasPlayer VidasPlayer;
+    private PlayerStadistics playerStadistics;
 
-    public int indexActual;
-
-    public Sprite bordecorazon;
-    public Sprite corazon;
-
-    private void Awake()
+    private void DesactivarVida(int indice)
     {
-        VidasPlayer.cambioVida.AddListener(CambiarCorazones);
-         
-    }
-    private void CambiarCorazones(int vidaActual)
-    {
-        if (!listaCorazones.Any())
-        {
-            CrearCorazones(vidaActual);
-        }
-        else
-        {
-            CambiarVida(vidaActual);
-        }
-    }
-
-    private void CambiarVida()
-    {
-        
-    }
-
-    private void CrearCorazones()
-    {
-        for (int i = 0)
+        playerStadistics = FindObjectOfType<PlayerStadistics>();
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
