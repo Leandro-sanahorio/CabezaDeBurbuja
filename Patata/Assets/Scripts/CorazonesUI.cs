7320e078057a0db0134453ef02a27cd8f4204780
Leandro <125918421+Leandro-sanahorio@users.noreply.github.com>
2025-01-25 19:45:11 -0500

    Cura y visualizacion
    se da por acabado la vida

--- Patata/Assets/Scripts/CorazonesUI.cs
+++ Patata/Assets/Scripts/CorazonesUI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 public class CorazonesUI : MonoBehaviour
 {
     public GameObject[] vidas;
+    public Image progressBar; // La imagen con "Fill"
 
     public void DesactivarVida(int indice){
         vidas[indice].SetActive(false);
@@ -15,4 +16,14 @@ public class CorazonesUI : MonoBehaviour
     public void ActivarVida(int indice){
         vidas[indice].SetActive(true);
     }
+
+    //Stand by
+    //private void UpdateProgressBar()
+    //{
+    //    if (progressBar != null)
+    //    {
+    //        progressBar.fillAmount  = (float)10 / 20; // Calcula el porcentaje
+    //    }
+    //}
+
 }
